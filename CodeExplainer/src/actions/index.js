c54fbee2696f3b462caa6ef473436765a959989c
Kaungsett45 <rrocket438@gmail.com>
2025-10-15 16:19:53 +0630

    NO response come back

--- CodeExplainer/src/actions/index.js
+++ CodeExplainer/src/actions/index.js
@@ -20,7 +20,9 @@ export async function explain(prevState, formData){
             }
 
             const data = await res.json();
-
+            console.log("Response status:", res.status);
+            console.log("Raw response code:", code);
+            console.log("Raw response code:", language);
             return{
                 success: true,
                 data
